b6ffc8a04c8b3cfa4ce5e2c285b7db25efe409e0
Joe Bowen <Joe.Bowen@sjrb.ca>
2019-05-23 12:38:01 -0600

    Added sample dynamodb support

--- handler.py
+++ handler.py
@@ -5,12 +5,33 @@ except ImportError:
     pass
 
 import json
+import os
+import boto3
+
+from botocore.config import Config as botoConfig
 
 
 def hello(event, context):
+    # This should handle rate limit issues with exponential back-off
+    # https://github.com/boto/boto3/issues/770
+    boto_config = botoConfig(
+        retries=dict(
+            max_attempts=50
+        )
+    )
+
+    dynamodb_client = boto3.client('dynamodb', config=boto_config)
+
+    message = dynamodb_client.get_item(
+        TableName=os.environ['MESSAGES_TABLE'],
+        Key={
+            'message_id': {'S': '0'}
+        }
+    )
+
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
+        "input": message['Item']['message']['S']
     }
 
     response = {
--- helper_scripts/deploy.sh
+++ helper_scripts/deploy.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+STAGE='prod'
+
 pip install -r requirements.txt
 
-serverless deploy -v
+serverless deploy -v --stage ${STAGE}
 
 SERVICE_ENDPOINT=$( cat client/stack_output.json | jq '.ServiceEndpoint' )
 
@@ -10,4 +12,6 @@ echo "REACT_APP_SERVICE_ENDPOINT=${SERVICE_ENDPOINT}" > client/.env
 
 ( cd client && npm run build )
 
-serverless client deploy --no-confirm
+serverless client deploy --no-confirm --stage ${STAGE}
+
+aws dynamodb update-item --table-name Messages-${STAGE} --key '{"message_id":{"S":"0"}}' --update-expression 'SET message = :message' --expression-attribute-values '{"message":{"S":"Test Dynamodb Message"}'
